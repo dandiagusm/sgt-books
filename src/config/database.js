bfd48bc86d687adc4b7a5a559489dc29adeb8921
Dandi <d.agusmaulana99@gmail.com>
2025-11-18 14:55:13 +0700

    added db con

---
+++ src/config/database.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool
+  .connect()
+  .then((client) => {
+    console.log("Connected to PostgreSQL");
+    client.release();
+  })
+  .catch((err) => {
+    console.error("❌ PostgreSQL connection error:", err.message);
+  });
+
+const query = (text, params) => {
+  return pool.query(text, params);
+};
+
+const getClient = async () => {
+  const client = await pool.connect();
+  return client;
+};
+
+module.exports = {
+  pool,
+  query,
+  getClient,
+};
